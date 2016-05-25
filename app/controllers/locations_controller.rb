3fe4917c6aa6b2ee87826955854cb94489701d7c
Saroj Sharma <saroj.webdeveloper@gmail.com>
2016-05-25 20:00:09 +0530

    location value passed

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,16 @@
 class LocationsController < ApplicationController
+
+def show
+  l= params[:search].downcase
+  @location = Location.find_by(name: l)
+
+end
+
+def query
+  # @location = Location.find_by(name: params[:search_by])
+
+   redirect_to location_path(@location)
+ end
+
+
 end
